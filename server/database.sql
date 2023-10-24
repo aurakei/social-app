67c14c640cebd7e6b809beeb4211046b0a714558
aurakei <aurakeith@gmail.com>
2023-10-24 12:10:29 +0300

    creating the first table user

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE socialdata;
 
 CREATE TABLE users(
-    user_id SERIAL PRIMARY KEY,
-    description VARCHAR (255)
+    user_id SERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR (255),
+    last_name VARCHAR (255),
+    user_name VARCHAR (255),
+    email VARCHAR (255),
+    password VARCHAR (255),
+    bio VARCHAR (255),
+    location VARCHAR (255),
+    join_date TIME,
+    date_of_birth DATE,
 );
